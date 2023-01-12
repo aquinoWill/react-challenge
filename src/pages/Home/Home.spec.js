0a40d97e3154bbd63b70744fa3dcaa6551d6eebf
Willian Aquino <willian.aquino@darwinseguros.com>
2023-01-12 14:57:43 -0300

    Home Page: Tests to check useEffect in click Sync components

--- src/pages/Home/Home.spec.js
+++ src/pages/Home/Home.spec.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-// import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
+import { screen } from '@testing-library/dom'
 import { Home } from './index'
 
 describe('Home Page', () => {
   it('Should render Home Page', () => {
     const { container } = render(<Home />)
     expect(container.querySelector('section')).toBeTruthy()
+  })
+
+  it('Should click in Img and change value sync', () => {
+    render(<Home />)
+    const img = screen.getByAltText(/Sync Contacts/i)
+    userEvent.click(img)
+    expect(screen.getByText(/All done!/i)).toBeInTheDocument()
   })
 })
