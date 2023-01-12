30c001ccbbde8f74ae7de7a0e06971d90197fb27
Willian Aquino <aquino.will@gmail.com>
2023-01-12 17:07:44 -0300

    Fix/layout position (#7)
    * Card and Home: refactory to move styles for Home page
    
    * MultiSelect: Animation when click to open
    
    * Layout: corretly position to cards and background image
    
    Co-authored-by: Willian Aquino <willian.aquino@darwinseguros.com>

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   width: 253px;
+  min-height: 355px;
   display: flex;
   align-items: center;
   background-color: #FFFFFF;
@@ -15,6 +16,7 @@ export const Wrapper = styled.div`
     border: 1px solid #D1DBE6;
     border-radius: 100px;
     padding: 20px;
+    margin-top: 30px;
     display: block;
     overflow: initial;
   }
